04f0162d105e0e4187d30a299021deed4340c24c
Dipavali Patel <shiny1702@gmail.com>
2020-12-21 15:08:54 +0530

    retrive current BAT balance from wallet

--- demo/swap.php
+++ demo/swap.php
@@ -69,35 +69,62 @@
   	
   	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script>
-
 	 
 $(document).ready(function(){
 
 	$("#btnAmount").prop('disabled', false);
 	$("#btnAmount").text("Enter an Amount");
 
-	$("#btnAmount").click( async function(){
-		
+	$("#txtFromToken").change(function(){
 		var vFromAmount = $('#txtFromToken').val();
   		window.web3 = new Web3(ethereum);
   		 web3.eth.getAccounts(async function(error, result) {
             if(!error && typeof(result[0]) !== 'undefined')
-                {
-                 metaMaskAddress=""+result[0];
-                var vCurrentBalance = await web3.eth.getBalance(metaMaskAddress);
-                var vAvailableETH =parseFloat( vCurrentBalance /  1e18);
-                if(parseFloat(vFromAmount) > parseFloat(vAvailableETH ))
-                {
-                	$("#btnAmount").text("Insuficient liquidity");
-                	$("#btnAmount").prop('disabled', true); 
-                }
+               {
+                	metaMaskAddress=""+result[0];
 
-                
-               // location.reload();
+                	var batContractAddress = "0xbf7a7169562078c96f0ec1a8afd6ae50f12e5a99";
+					var batABI = [{"constant":true,"inputs":[],"name":"batFundDeposit","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"batFund","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"tokenExchangeRate","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finalize","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"version","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"refund","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"tokenCreationCap","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"isFinalized","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"fundingEndBlock","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"ethFundDeposit","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"createTokens","outputs":[],"payable":true,"type":"function"},{"constant":true,"inputs":[],"name":"tokenCreationMin","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"fundingStartBlock","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"_ethFundDeposit","type":"address"},{"name":"_batFundDeposit","type":"address"},{"name":"_fundingStartBlock","type":"uint256"},{"name":"_fundingEndBlock","type":"uint256"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"LogRefund","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"CreateBAT","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}];
+
+              		var batContract = new web3.eth.Contract(batABI, batContractAddress, {
+		  					from: metaMaskAddress, // default from address
+		  				});
+
+              		var vCurrencyType = $('#spnFromToken').text();
+              		var vAvailableBalance =0;
+              		// this gives BAT amount
+              		var vCurrentBATbalance =await batContract.methods.balanceOf(metaMaskAddress).call();
+
+              		// this gives ETH amount
+                	var vCurrentETHBalance = await web3.eth.getBalance(metaMaskAddress);
+
+              		if(vCurrencyType=="BAT")
+              		{
+              			vAvailableBalance =parseFloat( vCurrentBATbalance /  1e18);
+              		}
+              		else if(vCurrencyType=="ETH")
+              		{
+              			vAvailableBalance =parseFloat( vCurrentETHBalance /  1e18);
+              		}
+
+                	if(parseFloat(vFromAmount) > parseFloat(vAvailableBalance ))
+                	{
+                		$("#btnAmount").text("Insuficient liquidity");
+                		$("#btnAmount").prop('disabled', true); 
+                	}
+                	else
+                	{
+                		$("#btnAmount").prop('disabled', false);
+						$("#btnAmount").text("Enter an Amount");
+                	}
+              }
+              else
+              {
+              	$('#txtFromToken').val('');
+              	$('#txtToToken').val('');
+              	alert("Please connect with your wallet.")
               }
             });
-  		//var bal = await web3.eth.getBalance('0x3F87A32Bfd9cf18B63290B752EAb5A54653B45C5');
-  		//alert(" current balance ="+bal);
   	});
 
 

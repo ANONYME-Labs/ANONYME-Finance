2f92dc21583de35e8af5bfc69b59dfdd5693c2c5
Dipavali Patel <shiny1702@gmail.com>
2020-12-28 15:28:32 +0530

    getting token values ABI and contract address from database table

--- demo/swap.php
+++ demo/swap.php
@@ -1,6 +1,10 @@
 <?php include 'header.php';?>
 <?php include 'sidebar.php';?>
 
+<?php
+	
+?>
+
 
   <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper" style="min-height: 1363.2px;">
@@ -53,6 +57,25 @@
                 <button id="btnAmount" class="btn btn-info btn-block btn-lg">Connect Wallet</button>
               </div>
             </div>
+            <?php
+            	$query2 = "SELECT * FROM currency ";
+				$result2 = mysqli_query($conn,$query2);
+				$vStr="";
+				if($result2){
+				while($row = mysqli_fetch_array($result2)){
+				
+					$name = $row['name'];
+					$mainContractAddress = $row['contractAddress'];
+					$mainContractABI = $row['contractABI'];
+					?>
+					<input type="hidden" id="txt_<?php echo $name; ?>" name="txt_<?php echo $name; ?>" value="<?php echo $name; ?>" class="tokenrow" >
+					<input type="hidden" id="txt_mc_<?php echo $name; ?>" name="txt_mc_<?php echo $name; ?>" value="<?php echo $mainContractAddress; ?>" class="tokenrow" >
+					<textarea style="visibility: hidden;" id="txt_abi_<?php echo $name; ?>"><?php echo $mainContractABI; ?></textarea>
+				<?php
+				}
+				
+				}else{ echo 'No result found!'; }
+            ?>
             <!-- /.card -->
           </div>
         </div>
@@ -103,30 +126,39 @@ $(document).ready(function(){
             if(!error && typeof(result[0]) !== 'undefined')
                {
                 	metaMaskAddress=""+result[0];
-
-                	var batContractAddress = "0xbf7a7169562078c96f0ec1a8afd6ae50f12e5a99";
-					var batABI = [{"constant":true,"inputs":[],"name":"batFundDeposit","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"batFund","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"tokenExchangeRate","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"finalize","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"version","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"refund","outputs":[],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"tokenCreationCap","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"isFinalized","outputs":[{"name":"","type":"bool"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"fundingEndBlock","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"ethFundDeposit","outputs":[{"name":"","type":"address"}],"payable":false,"type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"success","type":"bool"}],"payable":false,"type":"function"},{"constant":false,"inputs":[],"name":"createTokens","outputs":[],"payable":true,"type":"function"},{"constant":true,"inputs":[],"name":"tokenCreationMin","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[],"name":"fundingStartBlock","outputs":[{"name":"","type":"uint256"}],"payable":false,"type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"type":"function"},{"inputs":[{"name":"_ethFundDeposit","type":"address"},{"name":"_batFundDeposit","type":"address"},{"name":"_fundingStartBlock","type":"uint256"},{"name":"_fundingEndBlock","type":"uint256"}],"payable":false,"type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"LogRefund","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"CreateBAT","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_from","type":"address"},{"indexed":true,"name":"_to","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"_owner","type":"address"},{"indexed":true,"name":"_spender","type":"address"},{"indexed":false,"name":"_value","type":"uint256"}],"name":"Approval","type":"event"}];
-
-              		var batContract = new web3.eth.Contract(batABI, batContractAddress, {
-		  					from: metaMaskAddress, // default from address
-		  				});
-
-              		//var vCurrencyType = $('#spnFromToken').text();
-              		var vCurrencyType = $('#drpFromToken option:selected').text();
+                	var vCurrencyType = $('#drpFromToken option:selected').text();
               		var vAvailableBalance =0;
-              		// this gives BAT amount
-              		var vCurrentBATbalance =await batContract.methods.balanceOf(metaMaskAddress).call();
+              		var vCurrentbalance=0;
+					var vContractAddress ='';
+					var vContractABI ='';
+					var vContract='';
+					
+					if($('#txt_mc_'+vCurrencyType).val()!=undefined)
+					{
+	              		vContractAddress = $('#txt_mc_'+vCurrencyType).val().toString(); // main contract address
+	              		vContractABI = $('#txt_abi_'+vCurrencyType).val().toString();
+	              		vContractABI = JSON.parse(vContractABI);
+	              		vContract = new web3.eth.Contract(vContractABI, vContractAddress, {
+			  					from: metaMaskAddress, // default from address
+			  				});
+	              		// this gives BAT amount
+	              		vCurrentbalance =await vContract.methods.balanceOf(metaMaskAddress).call();
+	              	}
 
-              		// this gives ETH amount
-                	var vCurrentETHBalance = await web3.eth.getBalance(metaMaskAddress);
-
-              		if(vCurrencyType=="BAT")
+	              	if(parseInt(vCurrentbalance==0))
+	              	{
+	              		$("#btnAmount").prop('disabled', false);
+						$("#btnAmount").text("Enter an Amount");
+	              	}
+              		
+              		if(vCurrencyType=="ETH")
               		{
-              			vAvailableBalance =parseFloat( vCurrentBATbalance /  1e18);
-              		}
-              		else if(vCurrencyType=="ETH")
-              		{
+              			// this gives ETH amount
+                		var vCurrentETHBalance = await web3.eth.getBalance(metaMaskAddress);
               			vAvailableBalance =parseFloat( vCurrentETHBalance /  1e18);
+              		}
+              		else{
+              			vAvailableBalance =parseFloat( vCurrentbalance /  1e18);
               		}
 
                 	if(parseFloat(vFromAmount) > parseFloat(vAvailableBalance ))

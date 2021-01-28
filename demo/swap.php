801099fd28a4dad4575aa56c2bbede3acf834b06
Dipavali Patel <shiny1702@gmail.com>
2021-01-28 22:55:53 +0530

    on swap button click : received exchanged address

--- demo/swap.php
+++ demo/swap.php
@@ -351,39 +351,69 @@ $(document).ready(function(){
    $("#btnAmount").click(function(){
 		var vFromVal = $('#drpFromToken option:selected').text();
 		var vToVal = $('#drpToToken option:selected').text();
+		var contractABI = '';
+    	var contractAddress = '';
+    	var devide_to = '';
+
+    	// get contract address & ABI for selected token
+		$.ajax({
+                type: "POST",
+                url: 'ajax/getCurrencyData.php',
+                data: {tokenname:vToVal },
+                dataType: "json",
+                success: function (res) {
+                	if (res.status == '1') {
+                		contractABI = res.data.contractABI;
+                    	contractAddress = res.data.contractAddress;
+                    	devide_to = '1e'+res.data.desimals;
+
+                    	console.log(" token ="+res.data.name);
+                    	console.log(" contract address = "+ contractAddress);
+                	}
+                },
+                error: function (result) {
+                    alert("Error");
+                }
+            });
+
 		 window.web3 = new Web3(ethereum);
 		 web3.eth.getAccounts(async function (error, result) {
 
 		 	var ETHmetaMaskAddress = result[0];
+		 	let vExchangeAddress='';
 		 	console.log(" my wallet address =" + ETHmetaMaskAddress);
 
 		 	// SWAP ETH to ERC20
 			if(vFromVal == "ETH")
 			{
-				 $.ajax({
-	                type: "POST",
-	                url: 'ajax/getCurrencyData.php',
-	                data: {tokenname:vToVal },
-	                dataType: "json",
-	                success: function (res) {
-	                	if (res.status == '1') {
-	                		var contractABI = res.data.contractABI;
-                        	var contractAddress = res.data.contractAddress;
-                        	var devide_to = '1e'+res.data.desimals;
+				setTimeout(function(){
+				      //alert("alert 2 = " + dt);
+				}, 20000);
+				 console.log("contractAddress = "+contractAddress);
 
-                        	console.log(" token ="+res.data.name);
-                        	console.log(" contract address = "+ contractAddress);
+				 // the addressFrom address that will send the test transaction
+            	const addressFrom =ETHmetaMaskAddress; //'0x0e364eb0ad6eb5a4fc30fc3d2c2ae8ebe75f245c'
+            	 // the Uniswap factory contract address
+					var addressTo = '0xf5D915570BC477f9B8D6C0E980aA81757A3AaC36'
+					var abi = '[{"name":"NewExchange","inputs":[{"type":"address","name":"token","indexed":true},{"type":"address","name":"exchange","indexed":true}],"anonymous":false,"type":"event"},{"name":"initializeFactory","outputs":[],"inputs":[{"type":"address","name":"template"}],"constant":false,"payable":false,"type":"function","gas":35725},{"name":"createExchange","outputs":[{"type":"address","name":"out"}],"inputs":[{"type":"address","name":"token"}],"constant":false,"payable":false,"type":"function","gas":187911},{"name":"getExchange","outputs":[{"type":"address","name":"out"}],"inputs":[{"type":"address","name":"token"}],"constant":true,"payable":false,"type":"function","gas":715},{"name":"getToken","outputs":[{"type":"address","name":"out"}],"inputs":[{"type":"address","name":"exchange"}],"constant":true,"payable":false,"type":"function","gas":745},{"name":"getTokenWithId","outputs":[{"type":"address","name":"out"}],"inputs":[{"type":"uint256","name":"token_id"}],"constant":true,"payable":false,"type":"function","gas":736},{"name":"exchangeTemplate","outputs":[{"type":"address","name":"out"}],"inputs":[],"constant":true,"payable":false,"type":"function","gas":633},{"name":"tokenCount","outputs":[{"type":"uint256","name":"out"}],"inputs":[],"constant":true,"payable":false,"type":"function","gas":663}]'
+					var token =contractAddress;
 
+					const contract = new web3.eth.Contract(JSON.parse(abi), addressTo);
+					const tx = contract.methods.createExchange(token).send({ 
+                            from: addressFrom,
+                            to: addressTo
+                          });
 
+				// **************  GET Exchange Token  ************
+		        var abi = '[{"name":"NewExchange","inputs":[{"type":"address","name":"token","indexed":true},{"type":"address","name":"exchange","indexed":true}],"anonymous":false,"type":"event"},{"name":"initializeFactory","outputs":[],"inputs":[{"type":"address","name":"template"}],"constant":false,"payable":false,"type":"function","gas":35725},{"name":"createExchange","outputs":[{"type":"address","name":"out"}],"inputs":[{"type":"address","name":"token"}],"constant":false,"payable":false,"type":"function","gas":187911},{"name":"getExchange","outputs":[{"type":"address","name":"out"}],"inputs":[{"type":"address","name":"token"}],"constant":true,"payable":false,"type":"function","gas":715},{"name":"getToken","outputs":[{"type":"address","name":"out"}],"inputs":[{"type":"address","name":"exchange"}],"constant":true,"payable":false,"type":"function","gas":745},{"name":"getTokenWithId","outputs":[{"type":"address","name":"out"}],"inputs":[{"type":"uint256","name":"token_id"}],"constant":true,"payable":false,"type":"function","gas":736},{"name":"exchangeTemplate","outputs":[{"type":"address","name":"out"}],"inputs":[],"constant":true,"payable":false,"type":"function","gas":633},{"name":"tokenCount","outputs":[{"type":"uint256","name":"out"}],"inputs":[],"constant":true,"payable":false,"type":"function","gas":663}]'
+		        
+		        const uniswap = new web3.eth.Contract(JSON.parse(abi), addressTo);
+		        async function call(transaction) {
+		              return await transaction.call({from: addressFrom});
+		        }
+		        vExchangeAddress = await call(uniswap.methods.getExchange(token));
+		        console.log(" exchange address = "+vExchangeAddress);
 
-
-
-	                	}
-	                },
-	                error: function (result) {
-	                    alert("Error");
-	                }
-	            });
 			}
 		});
   	});
@@ -410,6 +440,7 @@ $(document).ready(function(){
             /*var startToken = $("#drpFromToken_title .ddlabel").html();
             var endToken = $("#drpToToken_title .ddlabel").html();*/
 
+           
             $.ajax({
                 type: "POST",
                 url: 'ajax/getCurrencyData.php',
@@ -448,16 +479,16 @@ $(document).ready(function(){
                                 }
 
                                 var path = [result, contractAddress];
-																amountOut = amountOut.toLocaleString('fullwide', {useGrouping:false});
-																console.log("amountOut : " + amountOut);
+								amountOut = amountOut.toLocaleString('fullwide', {useGrouping:false});
+								console.log("amountOut : " + amountOut);
 
-																console.log("path : " + path);
-																	//	var getamntin = routerContract.methods.getAmountsIn(amountOut, path).call();
-																	if(spnPoolFromToken=='ETH'){
-																			var getamntin = routerContract.methods.getAmountsOut(amountOut, path).call();
-																	} else {
-																			var getamntin = routerContract.methods.getAmountsIn(amountOut, path).call();
-																	}
+								console.log("path : " + path);
+									//	var getamntin = routerContract.methods.getAmountsIn(amountOut, path).call();
+									if(spnPoolFromToken=='ETH'){
+											var getamntin = routerContract.methods.getAmountsOut(amountOut, path).call();
+									} else {
+											var getamntin = routerContract.methods.getAmountsIn(amountOut, path).call();
+									}
 
 
                                 getamntin.then(function(getAmtVal) {
@@ -474,13 +505,13 @@ $(document).ready(function(){
                                     //{
                                     //  var ETHValue = getAmtVal[0];
                                     //  var  tokenAount= getAmtVal[1];
-																		if(spnPoolToToken=='ETH' ) {
-																				var ETHValue = getAmtVal[0];
-																				var  tokenAount= getAmtVal[1];
-																		} else {
-																				var ETHValue = getAmtVal[1];
-																				var  tokenAount= getAmtVal[0];
-																		}
+									if(spnPoolToToken=='ETH' ) {
+											var ETHValue = getAmtVal[0];
+											var  tokenAount= getAmtVal[1];
+									} else {
+											var ETHValue = getAmtVal[1];
+											var  tokenAount= getAmtVal[0];
+									}
                                       var inpDevide = (ETHValue/devide_to).toFixed(8);
                                       var getInpSingle = parseFloat(inpDevide).toFixed(8);
 
